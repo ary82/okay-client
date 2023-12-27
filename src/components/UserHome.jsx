b836d85292bb1b89d22212046923db893c0ebbd6
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2023-12-28 03:03:44 +0530

    display all usernames on userHome page

--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logout from "./Logout";
+import axios from "axios";
 
 export default function UserHome() {
+  const [users, setusers] = useState([]);
+  const getUsernames = () => {
+    axios({
+      method: "get",
+      url: "http://localhost:3000/users",
+    }).then((res) => {
+      setusers(res.data);
+    })
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    getUsernames();
+  }, []);
+
   return (
     <>
       <div>UserHome</div>
       <Logout />
+      <ul>
+        {users.map((user) => <li key={user._id}>{user.username}</li>)}
+      </ul>
     </>
   );
 }
